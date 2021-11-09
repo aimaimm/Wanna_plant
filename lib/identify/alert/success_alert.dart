a59d58de96f0aa2ab71942bdb30fc6d1cec085c5
Thidawan <aimaim.3112@gmail.com>
2021-11-09 18:34:53 +0700

    alert

---
+++ lib/identify/alert/success_alert.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:wanna_plant/identify/waitIdentifyScreen.dart';
+
+Future successAlert(BuildContext context) async {
+  await showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Icon(
+          Icons.check_circle,
+          size: 60,
+          color: Colors.green,
+        ),
+        content: SizedBox(
+          height: MediaQuery.of(context).size.height * 0.1,
+          child: Column(
+            children: [
+              Text(
+                'Success',
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(
+                height: 8,
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width,
+                child: TextButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => WaitIdentifyScreen(),
+                      ),
+                    );
+                  },
+                  child: Text(
+                    'OK',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                  style: TextButton.styleFrom(
+                    side: BorderSide(color: Colors.grey, width: 0.5),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+    },
+  );
+}
